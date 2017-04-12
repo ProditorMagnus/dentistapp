af57b94f57b6bf2524de29a032be736b2cad6d05
ProditorMagnus <public.ravana@gmail.com>
2017-04-12 21:13:48 +0300

    Pages load now

--- src/main/java/com/cgi/dentistapp/controller/DentistAppController.java
+++ src/main/java/com/cgi/dentistapp/controller/DentistAppController.java
@@ -1,5 +1,6 @@
 package com.cgi.dentistapp.controller;
 
+import com.cgi.dentistapp.dao.entity.DentistVisitEntity;
 import com.cgi.dentistapp.dto.DentistVisitDTO;
 import com.cgi.dentistapp.service.DentistVisitService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +8,24 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.validation.Valid;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Controller
 @EnableAutoConfiguration
 public class DentistAppController extends WebMvcConfigurerAdapter {
+    public static final int openH = 10;
+    public static final int closeH = 18;
+    public static final int appointmentsPerH = 2;
+    public static final int appointmentLen = 60 / appointmentsPerH;
 
     @Autowired
     private DentistVisitService dentistVisitService;
@@ -36,14 +45,67 @@ public class DentistAppController extends WebMvcConfigurerAdapter {
         if (bindingResult.hasErrors()) {
             return "form";
         }
-        // TODO validation
+        if (invalidDate(dentistVisitDTO)) {
+            bindingResult.addError(new FieldError("visitTime", "visitTime", "Vigane kuupäev"));
+        }
+        if (invalidTime(dentistVisitDTO)) {
+            bindingResult.addError(new FieldError("visitTimeH", "visitTimeH", "Vigane kellaaeg"));
+        }
+        if (bindingResult.hasErrors()) {
+            return "form";
+        }
         dentistVisitService.addVisit(dentistVisitDTO.getDentistName(), dentistVisitDTO.getDocName(), dentistVisitDTO.getVisitTime(), dentistVisitDTO.getVisitTimeH());
         return "redirect:/results";
     }
 
+    private boolean invalidTime(DentistVisitDTO dentistVisitDTO) {
+        try {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(dentistVisitDTO.getVisitTimeH());
+            if (calendar.get(Calendar.HOUR_OF_DAY) > closeH || calendar.get(Calendar.HOUR_OF_DAY) < openH) {
+                return true;
+            }
+            if (calendar.get(Calendar.MINUTE) % appointmentLen != 0) {
+                return true;
+            }
+        } catch (Exception e) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean invalidDate(DentistVisitDTO dentistVisitDTO) {
+        try {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(dentistVisitDTO.getVisitTime());
+            if (calendar.before(Calendar.getInstance())) {
+                return true;
+            }
+        } catch (Exception e) {
+            return true;
+        }
+        return false;
+    }
+
     @GetMapping("/view")
-    public String showRegistrations(ModelMap model) {
+    public String showRegistrations(DentistVisitDTO dentistVisitDTO, ModelMap model) {
         model.addAttribute("visits", dentistVisitService.listVisits());
         return "view";
+    }
+
+    @PostMapping("/view")
+    public String searchRegistrations(DentistVisitDTO dentistVisitDTO, ModelMap model) {
+        model.addAttribute("visits", dentistVisitService.listVisits().stream().filter(dentistVisitEntity -> searchMatches(dentistVisitDTO, dentistVisitEntity)).collect(Collectors.toList()));
+        return "view";
+    }
+
+    private boolean searchMatches(DentistVisitDTO dentistVisitDTO, DentistVisitEntity dentistVisitEntity) {
+        Date visitTime = dentistVisitDTO.getVisitTime();
+        Date visitTimeH = dentistVisitDTO.getVisitTimeH();
+        String dentistName = dentistVisitDTO.getDentistName();
+        String docName = dentistVisitDTO.getDocName();
+//        if (docName != null && dentistVisitEntity.getDocName() != null && !docName.equals(dentistVisitEntity.getDocName()))
+//            return false;
+        return true;
     }
 }
